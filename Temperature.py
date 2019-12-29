da0dbef6447474e17130e64378d320801e6cc360
sgw1999 <59341733+sgw1999@users.noreply.github.com>
2019-12-29 13:10:51 -0500

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,4 @@
+fahrenheit_input = input("Give me a number to convert from fahrenheit to celsius! ")
+centigrade = (fahrenheit_input-32)*5.0/9.0
+name = "Adalia Hiltebeital"
+print(str(name) + " " + str(centigrade));
